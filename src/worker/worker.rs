89d79440754d5b097179919ffe5cf54b8b3ded0e
dancespiele <spieljs@gmail.com>
2019-09-26 00:36:52 +0200

    create worker trait

--- src/worker/worker.rs
+++ src/worker/worker.rs
@@ -1,5 +1,9 @@
 use super::super::middlewares::helpers::MalagaMdw;
+use malaga_http_utils::utils
 
 pub trait Worker<MalagaMdw>{
-	fn new() -> Self;
+	fn new(mdw: MalagaMdw) -> Self;
+	fn get_request()-> Self;
+	fn execute_middlewares()->Self;
+	fn response_to_provider()->Self;
 }
